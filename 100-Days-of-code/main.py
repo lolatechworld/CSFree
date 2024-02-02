3c39a1f0f7f686815290f5873d1f9049db7dc546
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-02 22:36:50 +0100

    main

--- 100-Days-of-code/main.py
+++ 100-Days-of-code/main.py
@@ -8,3 +8,11 @@ print("String Concatenation is done with the \"+\" sign.")
 print('"String Concatenation is done with the "+" sign."')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+# Provide any name in the input pane below.
+# Get the name as input from the user
+name = input()
+character_count = len(name)
+print(character_count)
+
+
