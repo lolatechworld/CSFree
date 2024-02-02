81b99b48970f17df129c999e7626974dc3e9b7fa
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-02 18:39:21 +0100

    main

---
+++ 100-Days-of-code/Day_1-10.py
@@ -0,0 +1,6 @@
+# Genrerating a brand name
+print("Welcome to a brand name generator")
+name = input("What is your name?\n")
+City = input("What's the name of the city you grew up in?\n")
+Pet = input("What's your pet's name?\n")
+print(f"{name},your band name could be {City} {Pet}")
