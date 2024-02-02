5fec5221077627860a2430ff4a42415f8e2813e5
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-02 09:58:42 +0100

    Conditional-Statement

--- For-and-While-Loop-CSFree/Conditional_Statement.py
+++ For-and-While-Loop-CSFree/Conditional_Statement.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
 #How to get access to elements in the score list
+#goals = [75, 69, 85, 70]
+
+#for goal in goals:
+#   print(f"Before: {goal}")
+    #for any goal less than 90, add 10 marks
+#   if goal < 80:
+#       goal += 10
+#        print(f"After: {goal}")
+        
+
+#How to get access to elements in the score list
 goals = [75, 69, 85, 70]
 
-for goal in goals:
-    print(f"Before: {goal}")
-    #for any goal less than 90, add 10 marks
-    if goal < 80:
-        goal = goal +10
-        goal += 10
-print(goal)
+#len(goals)
+#range(len(goals));
+for i in range (len(goals)):#will print the index of the number
+    print(f"{i} = {goals[i]}")
+    
+    
