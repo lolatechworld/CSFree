ad8d9e5c69ffcba62d833a9230cc8c39df4d0df1
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-04 01:05:45 +0100

    concatination

--- 100-Days-of-code/Day_1.py
+++ 100-Days-of-code/Day_1.py
@@ -4,3 +4,5 @@ name = input("What is your name?\n")
 City = input("What's the name of the city you grew up in?\n")
 Pet = input("What's your pet's name?\n")
 print(f"{name},your band name could be {City} {Pet}")
+# Concatination
+print("Hello" + " " + "World!")
