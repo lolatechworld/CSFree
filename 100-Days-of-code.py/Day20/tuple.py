8b1045947653154c9b51a1fcff13635da45d43e1
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-19 00:03:46 +0100

    data structure

---
+++ 100-Days-of-code.py/Day20/tuple.py
@@ -0,0 +1,14 @@
+name = ["damy" "bello", 28, 34, 5.5]
+mylist = [2.4, "tobi", "yusuf", 23]
+num = [2, 3, 4, 5, 6]
+
+print(len(name))
+print(sorted(num))
+num.append(45)
+print(num)
+num.extend([2, 3])
+print(num)
+num.insert(1, 56)
+print(num)
+num[::-1]
+num = print(num)
