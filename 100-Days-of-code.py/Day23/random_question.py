b87b6cc72c46c02e1dca2e69933e23df22b51025
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-21 23:30:55 +0100

    task

---
+++ 100-Days-of-code.py/Day23/random_question.py
@@ -0,0 +1,12 @@
+import random
+
+questions = [
+    "What is your favorite color?",
+    "If you could travel anywhere in the world, where would you go?",
+    "What's your go-to comfort food?",
+    "If you could have any superpower, what would it be?",
+    "What hobby or activity do you enjoy in your free time?",
+]
+
+random_question = random.choice(questions)
+print(random_question)
