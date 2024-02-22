1d9f331e32edc26f3be49f56abe12887771b50c3
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-22 21:30:23 +0100

    signin/ signup portal

--- 100-Days-of-code.py/Day23/sign_in.py
+++ 100-Days-of-code.py/Day23/sign_in.py
@@ -1,7 +1,7 @@
 print("Welcome to Tech-titans portal")
-user_data = {}  # Dictionary to store user dat
 print("type ctrl d or ctrl c to quit the program")
 
+user_data = {}  # Dictionary to store user data
 storage = '.database'
 import json
 
@@ -25,30 +25,45 @@ def signup():
 
             # open a file and write
 
-            with open(storage, 'a') as db:
+            with open(storage, "w") as db:
                 # db.write(str(user_data))
                 json.dump(user_data, db, indent=2)
 
             print(f"Successfully signed up {user_info['username']}")
         elif choice.lower() == 'sign in':
-            print('signed in')
+           signin()
         else:
             print('Invalid Input! Try Again')
+
+        continue_form = input("Do you want to signin? ")
+        if continue_form.lower() == "signin":
+            signup()
     except (KeyboardInterrupt, EOFError):
         print("\nGoodbye!")
 
 def signin():
+
     try:
         signin_choice = input("Your username: ")
         pass_choice = input("Your password: ")
-    except ValueError:
-        print(f"Goodbye!")
 
+        with open(storage, "r") as db:
+            user_data = json.load(db)
+            if signin_choice in user_data:
+                if user_data[signin_choice]['password'] == pass_choice:
+                    print(f"Welcome back.{signin_choice}")
+                else:
+                    print("Incorrect password try again.")
+            else:
+                print("This user in not found sign up to Tech-titans portal")     
+    except (KeyboardInterrupt, EOFError):
+        print(f"Goodbye!")
 
 try:
     signup()
+
 except ValueError as ve:
-    print(f'Error! ve')
+    print("Error!")
 
 
 # create a sign in button in the elif choce and use the 
