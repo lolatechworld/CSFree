e40e8c9e25e1117e98a0de8d1ea4c45c6dcc877e
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-23 21:42:46 +0100

    Nested list

---
+++ 100-Days-of-code.py/Day24/Nested_list.py
@@ -0,0 +1,14 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", 
+                     "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", 
+                     "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", 
+                     "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", 
+                     "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",]
+num_state = len(states_of_america)
+print(states_of_america[num_state -5])
+
+names = ["milo", "lola", "momo"]
+other_names = ["Tobi", "ludu"]
+
+all_names = [names, other_names]
+
+print(all_names)
