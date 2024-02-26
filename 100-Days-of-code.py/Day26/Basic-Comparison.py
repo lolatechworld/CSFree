7318c471b3acf625fc05fc1559cc7b823ecb83ae
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-26 23:35:45 +0100

    comparison

---
+++ 100-Days-of-code.py/Day26/Basic-Comparison.py
@@ -0,0 +1,7 @@
+# If the temperature is above 30 degrees Celsius, should we activate the cooling system?
+# def temp():
+temp = int(input("What is the temperature: "))
+if temp > 30:
+    print("Should we activate the cooling system?")
+else:
+    print("Invalid input enter your degree above 30")
