7f2522ef9d3d9b9f86610433f6197182f105fa75
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-27 20:40:14 +0100

    updated

--- 100-Days-of-code.py/Day26/Basic-Comparison.py
+++ 100-Days-of-code.py/Day26/Basic-Comparison.py
@@ -1,7 +1,18 @@
 # If the temperature is above 30 degrees Celsius, should we activate the cooling system?
-# def temp():
+
 temp = int(input("What is the temperature: "))
 if temp > 30:
     print("Should we activate the cooling system?")
 else:
-    print("Invalid input enter your degree above 30")
+    print("Temperature is below 30 degrees. Cooling system activation not required.")
+
+# using the define function
+def temp(temp_degree):
+
+    if temp_degree > 30:
+       return("Should we activate the cooling system?")
+    else:
+        return("Temperature is below 30 degrees. Cooling system activation not required.")
+temp_degree = float(input("Enter the temperature in degrees Celsius: "))
+
+print(temp(temp_degree))
