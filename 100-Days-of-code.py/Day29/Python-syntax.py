af158a165b8d309d7a7a8adb2c803d86f446ffa6
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-29 00:45:18 +0100

    syntax-comment

---
+++ 100-Days-of-code.py/Day29/Python-syntax.py
@@ -0,0 +1,38 @@
+# Python syntax can be executed by writing directly in the Command Line
+# >>> print("Hello, World!")
+# Hello, World!
+
+# Or by creating a python file on the server, using the .py file extension, 
+# and running it in the Command Line
+print("Hello, World!")
+
+# Indentation refers to the spaces at the beginning of a code line.
+# Where in other programming languages the indentation in code is for 
+# readability only, the indentation in Python is very important.
+# Python uses indentation to indicate a block of code.
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# Python Variables are created when you assign a value to it
+x = 5
+y = "Hello, World!"
+
+# Comments is used to explain Python code, make the code more readable, and prevent execution when testing code.
+#  it starts with a #, and Python will ignore them.
+#This is a comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+#This is a comment
+#written in
+#more than just one line also known as multi line comment
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, lola!")
